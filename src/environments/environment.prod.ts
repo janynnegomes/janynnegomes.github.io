520f91a9284ce972daa158b0228329d14280649b
Janynne Gomes <janynnegomes@gmail.com>
2021-06-22 00:48:29 -0300

    fix environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: true,
   site: {
@@ -13,8 +9,46 @@ export const environment = {
     url: "http://janynnegomes.github.io",
     twitter_username: 'janynnegomes',
     github_username: 'janynnegomes',
-    mini_bio: 'Engenheira de Software com mais de 11 anos de experiência em desenvolvimento de sistemas. Atuação internacional em estágio de robótica na NASA no Centro de Vôos Espaciais Goddard nos EUA. Sólidos conhecimentos em UML, OOP, Javascript, Typescript, Angular, MySQL, Ionic, WordPress e Scrum. Forte relacionamento interpessoal com facilidade de comunicação em inglês e português',
+    mini_bio: 'Engenheira de Software com mais de 13 anos de experiência em desenvolvimento de sistemas. Atuação internacional em estágio de robótica na NASA no Centro de Vôos Espaciais Goddard nos EUA. Sólidos conhecimentos em UML, OOP, Javascript, Typescript, Angular, MySQL, Ionic, WordPress e Scrum. Forte relacionamento interpessoal com facilidade de comunicação em inglês e português',
     contact: 'disqus',
-    disqus_shortname: 'devnetgomez-github-io'
+    disqus_shortname: 'devnetgomez-github-io',
+    time: '2021',
+    data:{
+      social:[
+        {
+          title: 'LinkedIn',
+          url:  'http://www.linkedin.com/in/janynnegomes',
+          icon: 'fab fa-linkedin'
+        },
+        {
+          title: 'Facebook',
+          url: 'https://www.facebook.com/DiarioDeUmaProgramadorA/',
+          icon: 'fab fa-facebook'
+        },
+        {
+          title: 'Twitter',
+          url: 'http://twitter.com/devnetgomez',
+          icon: 'fab fa-twitter'
+        },
+        {
+          title: 'Blog',
+          url: 'http://janynnegomes.wordpress.com',
+          icon: 'fab fa-wordpress'
+        },
+        {
+          title: 'Github',
+          url: 'http://github.com/devnetgomez',
+          icon: 'fab fa-github'
+        },
+        {
+          title: 'Instagram',
+          url: 'https://instagram.com/janynnegomesdev/',
+          icon: 'fab fa-instagram'
+        }
+      ],
+      footer:{
+        copyright:""
+      }
+    }
   }
 };
